7183a7b60c204fc8a2f3544946ce0ffec328aa5c
Sean Mulligan <sean.mulligan85@gmail.com>
2015-05-10 16:36:42 -0700

    Adding api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :api, defaults: { format: :json } do
+  resources :events, only: [:create]
+  end
+
   resources :registered_applications
   
   root 'welcome#index'
