20059eb5c5bec86ca9f447866bbce2069af40eaa
Sean Mulligan <sean.mulligan85@gmail.com>
2015-05-11 07:34:25 -0700

    Adding js snippet for event tracking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $('a').click(function(e) {
+    Metrics.report("User clicks: " + e.target);
+  });
+});
+
+
+var Metrics = {
+  report: function(event) {
+var _bm_request = new XMLHttpRequest();
+_bm_request.open("POST", "http://localhost:3000/api/events", true);
+_bm_request.setRequestHeader('Content-Type', 'application/json');
+// _bm_request.onreadystatechange = function() {
+// };
+_bm_request.send(JSON.stringify({event: {name: event}}));
+  }
+}
